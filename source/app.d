9bc3bac1f8d725c78d79d7a61f156726e9692909
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-04-21 23:48:36 +0300

    some info structs moved to defaults

--- source/app.d
+++ source/app.d
@@ -135,36 +135,6 @@ void main() {
     viewportState.scissorCount = 1;
     viewportState.pScissors = &scissor;
 
-    VkPipelineMultisampleStateCreateInfo multisampling;
-    multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-    multisampling.sampleShadingEnable = VK_FALSE;
-    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
-    multisampling.minSampleShading = 1.0f; // Optional
-    multisampling.pSampleMask = null; // Optional
-    multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
-    multisampling.alphaToOneEnable = VK_FALSE; // Optional
-
-    VkPipelineColorBlendAttachmentState colorBlendAttachment;
-    colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
-    colorBlendAttachment.blendEnable = VK_FALSE;
-    colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
-    colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
-    colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD; // Optional
-    colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
-    colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
-    colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD; // Optional
-
-    VkPipelineColorBlendStateCreateInfo colorBlending;
-    colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
-    colorBlending.logicOpEnable = VK_FALSE;
-    colorBlending.logicOp = VK_LOGIC_OP_COPY; // Optional
-    colorBlending.attachmentCount = 1;
-    colorBlending.pAttachments = &colorBlendAttachment;
-    colorBlending.blendConstants[0] = 0.0f; // Optional
-    colorBlending.blendConstants[1] = 0.0f; // Optional
-    colorBlending.blendConstants[2] = 0.0f; // Optional
-    colorBlending.blendConstants[3] = 0.0f; // Optional
-
     VkDynamicState[] dynamicStates = [
         VK_DYNAMIC_STATE_VIEWPORT,
         VK_DYNAMIC_STATE_SCISSOR,
--- source/pukan/vulkan/defaults.d
+++ source/pukan/vulkan/defaults.d
@@ -31,3 +31,33 @@ VkPipelineRasterizationStateCreateInfo rasterizer = {
     depthBiasClamp: 0.0f, // Optional
     depthBiasSlopeFactor: 0.0f, // Optional
 };
+
+VkPipelineMultisampleStateCreateInfo multisampling = {
+    sType: VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+    sampleShadingEnable: VK_FALSE,
+    rasterizationSamples: VK_SAMPLE_COUNT_1_BIT,
+    minSampleShading: 1.0f, // Optional
+    pSampleMask: null, // Optional
+    alphaToCoverageEnable: VK_FALSE, // Optional
+    alphaToOneEnable: VK_FALSE, // Optional
+};
+
+shared VkPipelineColorBlendAttachmentState colorBlendAttachment = {
+    colorWriteMask: VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT,
+    blendEnable: VK_FALSE,
+    srcColorBlendFactor: VK_BLEND_FACTOR_ONE, // Optional
+    dstColorBlendFactor: VK_BLEND_FACTOR_ZERO, // Optional
+    colorBlendOp: VK_BLEND_OP_ADD, // Optional
+    srcAlphaBlendFactor: VK_BLEND_FACTOR_ONE, // Optional
+    dstAlphaBlendFactor: VK_BLEND_FACTOR_ZERO, // Optional
+    alphaBlendOp: VK_BLEND_OP_ADD, // Optional
+};
+
+VkPipelineColorBlendStateCreateInfo colorBlending = {
+    sType: VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+    logicOpEnable: VK_FALSE,
+    logicOp: VK_LOGIC_OP_COPY, // Optional
+    attachmentCount: 1,
+    pAttachments: &cast(VkPipelineColorBlendAttachmentState) colorBlendAttachment,
+    blendConstants: [0.0f, 0.0f, 0.0f, 0.0f], // Optional
+};
