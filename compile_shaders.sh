6e80e001e7b9bae2dad49384ff924d922b08597f
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-04-22 16:14:04 +0300

    shaders/ moved to pukan/

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-glslc -fshader-stage=vertex source/shaders/vertices.glsl -o vert.spv
+glslc -fshader-stage=vertex source/pukan/shaders/vertices.glsl -o vert.spv
 
-glslc -fshader-stage=frag source/shaders/fragment.glsl -o frag.spv
+glslc -fshader-stage=frag source/pukan/shaders/fragment.glsl -o frag.spv
