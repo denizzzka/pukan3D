b125696c9b1c7a81a33ef606506db29671b0ad4f
Denis Feklushkin <feklushkin.denis@gmail.com>
2025-05-01 21:23:18 +0300

    class ImageMemory moved to image module

--- source/pukan/vulkan/package.d
+++ source/pukan/vulkan/package.d
@@ -3,6 +3,7 @@ module pukan.vulkan;
 public import pukan.vulkan.commands;
 public import pukan.vulkan.core;
 public import pukan.vulkan.frame;
+public import pukan.vulkan.image;
 public import pukan.vulkan.logical_device;
 public import pukan.vulkan.memory;
 public import pukan.vulkan.pipelines;
